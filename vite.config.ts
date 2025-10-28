e3e5474f817888876b0c23ff9d29b60fa8814737
Alisa <cheshyr.alisa@gmail.com>
2025-10-28 22:11:29 +0200

    readme added

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 
-// Якщо деплоїш у репозиторій user/repo, і сторінка буде за адресою
-// https://user.github.io/repo/ — додай base: "/repo/".
-// Якщо це user.github.io (user site) — base можна не вказувати.
 export default defineConfig({
-  base: '/space-shooter/', // ← заміни на назву свого репозиторію або прибери
+  base: '/space-shooter/',
 });
